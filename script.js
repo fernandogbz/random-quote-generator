268a0c94ac6d700d6acef92eecc18c971903c920
Fernando Jimenez <fgjv06@gmail.com>
2023-07-20 23:31:29 -0400

    JS - random quote function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const quoteText = document.querySelector(".quote"),
 quoteBtn = document.querySelector("button");
 
+// random quote function
+function randomQuote() {
+  console.log("Clicked");
+}
+
 quoteBtn.addEventListener("click", randomQuote);
