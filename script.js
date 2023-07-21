1f661e2ec8919a2b9117d937f341b7a2702aa7ca
Fernando Jimenez <fgjv06@gmail.com>
2023-07-20 23:37:26 -0400

    JS - fetching random quotes/data from the API and parsing it into JavaScript object

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ quoteBtn = document.querySelector("button");
 
 // random quote function
 function randomQuote() {
-  console.log("Clicked");
+  // fetching random quotes/data from the API and parsing it into JavaScript object
+  fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+    console.log(result)
+  });
 }
 
 quoteBtn.addEventListener("click", randomQuote);
