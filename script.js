c7b1bb4d550a773eaa6e33f8c78bb34d7a95c364
Fernando Jimenez <fgjv06@gmail.com>
2023-07-24 23:57:37 -0400

    JS - less opacity when quoteBtn is clicked

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ quoteBtn = document.querySelector("button");
 // random quote function
 function randomQuote() {
   quoteBtn.classList.add("loading");
-  quoteBtn.innerText = "Loading Quote...";
+  quoteBtn.innerText = "Loading...";
   // fetching random quotes/data from the API and parsing it into JavaScript object
   fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
     console.log(result);
     quoteText.innerText = result.content; // Shows quote
     authorName.innerText = result.author; // Shows author
     quoteBtn.innerText = "New Quote";
+    quoteBtn.classList.remove("loading");
   });
 }
 
